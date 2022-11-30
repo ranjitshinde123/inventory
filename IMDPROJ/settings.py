a7fe73888a87dc8e2c4d0a604bce0b535573d91f
Monika <monikagavate5@gmail.com>
2022-11-30 12:25:03 +0530

    host

--- IMDPROJ/settings.py
+++ IMDPROJ/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-9(qoxn594k57a^ie(rt225liumt$(7_r2^ej2fbj*q#zgybt0s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
