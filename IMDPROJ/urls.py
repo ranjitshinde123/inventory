9978ae19513d9611258bd16559b36edbd6457e6f
Monika <monikagavate5@gmail.com>
2022-12-06 11:09:58 +0530

    stock

--- IMDPROJ/urls.py
+++ IMDPROJ/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 
     path('admin/', admin.site.urls, name='admin'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('', include('homepage.urls')),
     path('inventory/', include('IMDAPP.urls')),
