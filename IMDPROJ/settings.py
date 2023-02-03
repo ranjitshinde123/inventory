c53d85eb2c141b92fc96b0419ae782251c1b5a2e
komal <komalp.mobicloud@gmail.com>
2023-02-03 11:57:03 +0530

    commit

--- IMDPROJ/settings.py
+++ IMDPROJ/settings.py
@@ -93,17 +93,27 @@ WSGI_APPLICATION = 'IMDPROJ.wsgi.application'
 # }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'eaeadmin',
+#         'USER': 'eaeuser',
+#         'PASSWORD': 'denny@123',
+#         'HOST':'localhost',
+#         'PORT':'5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'testdb_pz7a',
-        'USER': 'testdb_pz7a_user',
-        'PASSWORD': 'xLCMrnJT4PZZc5hvuPmUFg6iuqiMoeJx',
-        'HOST':'dpg-cfe8p41mbjsqnjli1gdg-a',
-        'PORT':'5432',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.sqlite3',
+
+        'NAME': BASE_DIR / 'db.sqlite3',
+
+    }
+
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
