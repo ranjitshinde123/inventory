8d772650b6ac46d2b503d7ad308a50850ced5c47
komal <komalp.mobicloud@gmail.com>
2023-02-03 12:00:44 +0530

    commit change

--- IMDPROJ/settings.py
+++ IMDPROJ/settings.py
@@ -88,9 +88,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'IMDPROJ.wsgi.application'
 
-# DATABASES = {
-#     'default' : dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
+DATABASES = {
+    "default" : dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # DATABASES = {
@@ -103,17 +103,7 @@ WSGI_APPLICATION = 'IMDPROJ.wsgi.application'
 #         'PORT':'5432',
 #     }
 # }
-DATABASES = {
 
-    'default': {
-
-        'ENGINE': 'django.db.backends.sqlite3',
-
-        'NAME': BASE_DIR / 'db.sqlite3',
-
-    }
-
-}
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
